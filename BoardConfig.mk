8248c6fcc040fe9b5b253fe855c6aca9ad45f5e5
Tim Pohlad-Thomas <timpohladthomas@gmail.com>
2014-02-19 10:27:02 -0500

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/motorola/moto_msm8960
 TARGET_OTA_ASSERT_DEVICE := moto_msm8960,xt925,xt926,xt907,vanquish_u,vanquish,scorpion_mini,mb886,qinara,asanti,asanti_c,xt897,xt897c
 
 # Recovery
-BOARD_CUSTOM_GRAPHICS := ../../../$(LOCAL_PATH)/graphics.c
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 BOARD_RECOVERY_SWIPE := true
