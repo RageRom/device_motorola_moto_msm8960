d04073cbd3b8bd8b5a2844f551fa0d7d34cb7919
jl90 <leonardjason90.jl@gmail.com>
2014-05-01 18:05:39 -0700

    Update and rename ose.mk to berserk.mk

---
+++ berserk.mk
@@ -0,0 +1,14 @@
+# Inherit some common Berserk stuff.
+$(call inherit-product, vendor/berserk/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/berserk/config/nfc_enhanced.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 720
+TARGET_SCREEN_HEIGHT := 1280
+
+# Release name
+PRODUCT_NAME := berserk_moto_msm8960
+
+$(call inherit-product, device/motorola/moto_msm8960/full_moto_msm8960.mk)
