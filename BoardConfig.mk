1fe268cebc5cb9e327bbf0eb867b9c31d548475f
jl90 <leonardjason90.jl@gmail.com>
2014-06-01 15:23:43 -0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,6 @@ TARGET_OTA_ASSERT_DEVICE := moto_msm8960,xt901,solstice,xt905,smq_u,xt907,scorpi
 
 # Recovery
 TARGET_RECOVERY_NO_MSM_BSP := true
-BOARD_CUSTOM_GRAPHICS := ../../../$(LOCAL_PATH)/graphics.c
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 BOARD_RECOVERY_SWIPE := true
